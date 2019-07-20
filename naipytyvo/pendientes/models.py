1a273198a2703a6bca05f27054fed317c93bddc0
Eudelio Carmona <alezcano09@gmail.com>
2019-07-21 00:41:19 +0200

    Agregue Modelos al sitio de administrador

--- naipytyvo/pendientes/models.py
+++ naipytyvo/pendientes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Info(models.Model):
+    nombre = models.TextField(max_length=20)
+    telefono = models.IntegerField(max_length=20)
+    ciudad = models.TextField(max_length=20)
+    servicio = models.TextField(max_length=20)
+    breve_descripcion = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
