acffb4ebc85da04b6df38a4a15c76d9dbcfb4d6c
Eudelio Carmona <alezcano09@gmail.com>
2019-07-22 04:58:41 +0200

    Modificamos para que pueda estirar los datos de registro

--- naipytyvo/pendientes/views.py
+++ naipytyvo/pendientes/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Info
 # Create your views here.
 def pagina_principal(request):
     return render(request, "index.html")
+
+def servicios(request):
+    datos = Info.objects.all()   #Consultamos nuestra base de datos
+    datos_enviados={'datos': datos}
+    print(datos_enviados)
+    return render(request, "servicios.html",datos_enviados) #enviamos los datos a la pagina
