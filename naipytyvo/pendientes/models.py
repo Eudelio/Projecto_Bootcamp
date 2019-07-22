742824c20623e78459d6234f832a335ca33838bc
Eudelio Carmona <alezcano09@gmail.com>
2019-07-22 16:05:56 +0200

    Modificamos Img. Settings, Models

--- naipytyvo/pendientes/models.py
+++ naipytyvo/pendientes/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 class Info(models.Model):
     nombre = models.TextField(max_length=20)
-    telefono = models.IntegerField(max_length=20)
+    telefono = models.IntegerField()
     ciudad = models.TextField(max_length=20)
-    servicio = models.TextField(max_length=20)
+    servicio = models.TextField(max_length=50)
     breve_descripcion = models.TextField(max_length=100)
+    imagen=models.ImageField(upload_to = 'imagenes/', default = 'imagenes/None/no-img.jpg')
 
     def __str__(self):
         return self.nombre
