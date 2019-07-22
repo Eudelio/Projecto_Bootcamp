2c2e581665fa3e7e96a152abdce746dce122c1d7
Eudelio Carmona <alezcano09@gmail.com>
2019-07-22 20:24:40 +0200

    Archivo modificado y funciona

--- naipytyvo/pendientes/models.py
+++ naipytyvo/pendientes/models.py
@@ -6,9 +6,10 @@ class Info(models.Model):
     nombre = models.TextField(max_length=20)
     telefono = models.IntegerField()
     ciudad = models.TextField(max_length=20)
-    servicio = models.TextField(max_length=50)
-    breve_descripcion = models.TextField(max_length=100)
+    servicio = models.TextField(max_length=100)
+    breve_descripcion = models.TextField(max_length=500)
     imagen=models.ImageField(upload_to = 'imagenes/', default = 'imagenes/None/no-img.jpg')
+    nro= models.IntegerField(default= 0)
 
     def __str__(self):
         return self.nombre
