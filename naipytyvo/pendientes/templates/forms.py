d7bcf525823070021b352291d81fe8d516f309b2
Eudelio Carmona <alezcano09@gmail.com>
2019-07-23 06:59:38 +0200

    Agregue un Archivo forms

---
+++ naipytyvo/pendientes/templates/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class formulario
+class Formulario (forms.from):
+    nombre = forms.CharFielt(max_length=100)
+    telefono = forms.IntegerField()
+    ciudad = forms.CharFielt(max_length=20)
+    servicios = forms.CharFielt(max_length=500)
+    breve_descripcion = forms.TextField(max_length=500)
